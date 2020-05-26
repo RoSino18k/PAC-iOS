2a5c58c488f0070ef7e2ffdc1245f71ddf01e8eb
Trần Văn Cầu <rosino999@gmail.com>
2020-05-26 12:31:48 +0700

    Update pac-antirevoke.js

--- pac-antirevoke.js
+++ pac-antirevoke.js
@@ -1,6 +1,6 @@
 var DIRECT = "DIRECT";
-var PROXY = "PROXY 127.0.0.1:80";
-var blacklist = {"ocsp.apple.com":1, "gdmf.apple.com":1};
+var PROXY = "PROXY 127.0.0.1:7979";
+var blacklist = {"ocsp.apple.com":1, "gdmf.apple.com":1, "mesu.apple.com":1};
 function FindProxyForURL(url, host) {
   host = host.toLowerCase();
   for (i = 0; i < 30; i++) {
